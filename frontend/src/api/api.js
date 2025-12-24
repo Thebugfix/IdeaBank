1151d6814322d9bbb5af6a2625459e0f7fb5ac62
Naved Shaikh <shaikhnaved0001@gmail.com>
2025-12-24 11:58:53 +0530

    api update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // <-- Critical
+  baseURL: 'https://idea-bank-smoky.vercel.app/api', // <-- Critical
   headers: {
     'Content-Type': 'application/json',
   },
